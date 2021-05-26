a9525892e027529ab21d8ce54ecd592bc43b1223
kagec <66302618+kagec@users.noreply.github.com>
2021-05-26 23:28:01 +0900

    test:修正　findAll コメントアウトを削除

--- test/models/findAll.test.ts
+++ test/models/findAll.test.ts
@@ -3,16 +3,6 @@ import {todo} from '../../models/todos';
 
 
 describe('findAll', () => {
-	// describe('failure', () => {
-	// 	test('fail', async () => {
-	// 		prismaMock.todo.findMany.mockReturnValue
-	// 	});
-	// });
-
-	// describe('success', () => {
-
-	// });
-
 	test('success', async () => {
 		await expect(todo.findAll()).resolves.toBeDefined;
 	});
